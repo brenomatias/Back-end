4838d100a1ba080d39716de74a4417176c23e90b
brenoMatias <matiasbreno19@gmail.com>
2022-01-11 04:34:23 -0300

    starting mocha

---
+++ block22-node-introduction/mocha-chai-sinon/unit-tests.js
@@ -0,0 +1,9 @@
+// Testes unitários : Trabalham presumindo um escopo limitado a um pequeno fragmento do seu código com interação mínima entre recursos externos. Ex: Uma função com um fim específico, como uma função que soma dois números:
+
+
+// ./funcoes/calculo/soma.js (estaríamos neste path)
+// Aqui podemos escrever testes pensando somente o comportamento esperado para função `soma`
+
+const soma = (valorA, valorB) => valorA + valorB;
+
+module.exports = soma;
