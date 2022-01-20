b96f873adf75d6626fcab6471025a74f5758d747
brenoMatias <matiasbreno19@gmail.com>
2022-01-20 13:58:32 -0300

    finishing

--- block23-node-architecture/MODEL-LAYER/example/index.js
+++ block23-node-architecture/MODEL-LAYER/example/index.js
@@ -4,11 +4,27 @@ const app = express();
 
 const Author = require('./models/author-queries');
 
+
+
 app.get('/authors', async (req, res) => {
      
     const authors = await Author.getAll();
 
     res.status(200).json(authors);
+});
+
+
+
+app.get('/authors/:id', async (req, res) => {
+
+    const { id } = req.params;
+     
+    const author = await Author.findById(id);
+
+    if(!author) return res.status(404).json({message: 'not found'});
+
+    res.status(200).json(author);
+
 })
 
 app.listen(3000, () => console.log('listening on 3000'));
