8d99ad89a0178c1d0a51e374f3b24357dcf6e668
brenoMatias <matiasbreno19@gmail.com>
2022-01-25 19:45:56 -0300

    create database

--- block23-node-architecture/REST-RESTFUL/index.js
+++ block23-node-architecture/REST-RESTFUL/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/products', require('./controllers/productController'));
 
