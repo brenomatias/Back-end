7c0b0ac25217fec8196d603107c8c4466a039123
brenoMatias <matiasbreno19@gmail.com>
2022-02-03 10:29:17 -0300

    adding index(main)

---
+++ block24-ORM-authentication/INTRODUCTION/exercises-sequelize-intro/controllers/bookController.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const { Book } = require('../models/index');
+// import the model of the db (note: index.js (auto genereated in sequelize init))
+
+const router = express.Router();
+
---
+++ block24-ORM-authentication/INTRODUCTION/exercises-sequelize-intro/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const userController = require('./controllers/bookController');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+app.use('/user', userController);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
