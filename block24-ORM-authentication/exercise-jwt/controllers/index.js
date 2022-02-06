ae83f3bead1ee1d2394a49c5450c8f81deafa0e5
brenoMatias <matiasbreno19@gmail.com>
2022-02-06 20:05:43 -0300

    changing login endpoint

---
+++ block24-ORM-authentication/exercise-jwt/controllers/index.js
@@ -0,0 +1,8 @@
+
+// 1.5)
+const login = require('./login');
+
+ module.exports = {
+  login,
+};
+// 1.5)
--- block24-ORM-authentication/exercise-jwt/controllers/login.js
+++ block24-ORM-authentication/exercise-jwt/controllers/login.js
@@ -1,10 +1,10 @@
-const Joi = require('joi') //1)
+const Joi = require('joi') //1.1)
 
-const jwt = require('jsonwebtoken'); //3)
+const jwt = require('jsonwebtoken'); //1.3)
 
-const { JWT_SECRET } = process.env; //3)
+const { JWT_SECRET } = process.env; //1.3)
 
-//1)
+//1.1)
 const validateBody = (body) => Joi.object({
       /* Utilizamos o Joi para validar o schema do body */
 
@@ -12,10 +12,10 @@ const validateBody = (body) => Joi.object({
     password: Joi.string().min(5).required(),
 
 }).validate(body);
-//1)
+//1.1)
 
 
-//1)
+//1.1)
 module.exports = async (req, res, next) => {
       /* Construímos um schema do Joi */
   const { error } = validateBody(req.body);
@@ -25,22 +25,41 @@ module.exports = async (req, res, next) => {
   if (error) return next(error);
 
 
-  //3) 
+//2.1)
+/* Se o login for admin e a senha estiver incorreta */
+    if (req.body.username === 'admin' && req.body.password !== 's3nh4S3gur4???') {
+      /* Criamos um novo objeto de erro */
+      const err = new Error('Invalid username or password');
+      /* Adicionamos o status `401 Unauthorized` ao erro */
+      err.statusCode = 401;
+      /* Passmos o erro para o express, para que seja tratado pelo middleware de erro */
+      return next(err);
+     }
+//2.1)
+
+//2.1)
+ /* Definimos admin como true se username e password estiverem corretos */
+ const admin = req.body.username === 'admin' && req.body.password === 's3nh4S3gur4???';
+//2.1)
+
+
+  //1.3) 
   const payload = {
     username: req.body.username,
-    admin: false,
+    // admin: false, 1.3
+    admin,//2.1
   };
-  //3)
+  //1.3)
 
-  //3)
+  //1.3)
   const token = jwt.sign(payload, JWT_SECRET, {
     expiresIn: '1h',
   });
-  //3)
+  //1.3)
    
-  //3)
-  res.status(200).json({ token });
-  //3)
+  //1.3)
+  return res.status(200).json({ token });
+  //1.3)
 
   }
-//1)
+//1.1)
