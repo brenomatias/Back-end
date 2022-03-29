d1679b69d1283672cc4cf6cbd539b1f939af5fa2
brenoMatias <matiasbreno19@gmail.com>
2022-03-29 08:58:28 -0300

    base to CRUD applied

---
+++ block26-typescript/CRUD-TS-express/src/models/book.model.ts
@@ -0,0 +1,38 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+
+import Book from '../interfaces/book.interface';
+// importar a interface Book. Ela continuará definindo o tipo de retorno esperado nas nossas requisições.
+
+
+// No TypeScript , as classes são uma maneira de definir a forma de um objeto, podemos considerar uma classe como PROJETO PARA CRIAÇÃO DE OBJETOS
+export default class BookModel {
+    public connection: Pool;
+
+    constructor(connection: Pool) {
+        this.connection = connection;
+    }
+
+    // getAll
+    public async getAll(): Promise<Book[]> {
+        const result = await this.connection
+          .execute('SELECT * FROM books');
+
+          const [rows] = result;
+        
+    return rows as Book[];
+    }
+
+    //create book
+    public async create(book: Book): Promise<Book> {
+        const { title, price, author, isbn } = book;
+
+        const result = await this.connection.execute<ResultSetHeader>(
+            'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)', [title, price, author, isbn],
+        );
+        
+        const [dataInserted] = result;
+        const { insertId } = dataInserted;
+        return { id: insertId, ...book };
+    }
+
+}
