b96a3b38b9a92fefcb078c32912dacfd2c570383
brenoMatias <matiasbreno19@gmail.com>
2022-04-06 03:47:42 -0300

    settling interfaces

--- block27-OOP-Solid/inheritances-interfaces/interfaces.ts
+++ block27-OOP-Solid/inheritances-interfaces/interfaces.ts
@@ -7,6 +7,7 @@ export interface IPartyMember {
     attack(): void;
 }
 
+// assinatura da interface - notação de 'I' para interface
 export interface IPlayer {
     pickUpItem(item: Item): void;
 }
--- block27-OOP-Solid/inheritances-interfaces/player.ts
+++ block27-OOP-Solid/inheritances-interfaces/player.ts
@@ -1,19 +1,37 @@
 import { IPartyMember, IPlayer, Item } from "./interfaces";
 import Race from "./race";
 
-export default class Player<T extends IPlayer> implements IPlayer, IPartyMember {
-    
+// o player vai ser de uma raça mas ele não precisa herdar tudo de raç, logo faz sentido o player RECEBER NA ISNTANCIA a classe
+
+// COMPOSIÇÃO
+// 'composição de classes' é reunir todos os tipos de objetos que você precisa em uma mesma classe ao invés de fazer herança:
+
+
+export default class Player<Race extends IPlayer> implements IPlayer, IPartyMember {
+
+// implements -> implementa a interface
+
     constructor(
         private _hp: number,
         private _mp: number,
-        private _player: T,
+        private _player: Race,
+        // compõe a classe player com um objeto do tipo Race
+        // player tem acesso a tudo que é publico de race aqui
+        // pela interface, 'Player' tem que ser algo que tem a função 
+        // 'pickUpItem' - entao pode ser genérico
     ) { }
 
     attack(): void {
         throw new Error("Method not implemented.");
     }
 
-    pickUpItem(item: Item): void {
+    // método que a INTERFACE exige
+       pickUpItem(item: Item): void {
         this._player.pickUpItem(item);
     }
 }
+
+// Utilizamos herança sempre que queremos especializar uma classe, ou seja, deixar ela mais específica. Basta se perguntar se subclasse é um tipo de superclasse : se a resposta for não, então não devemos utilizar herança.
+
+//         private _player: Race,
+// assim o atributo depende de um objeto de tipo race e nao somente a necessidade da funçao 'pickUpItem' como exige na interface
