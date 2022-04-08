5abe0ebecaf7c240975ca46cd20abb1138329aa5
brenoMatias <matiasbreno19@gmail.com>
2022-04-08 14:03:08 -0300

    S principle applied

--- block27-OOP-Solid/SOD-Principles/controllers/userController.ts
+++ block27-OOP-Solid/SOD-Principles/controllers/userController.ts
@@ -3,16 +3,22 @@ import User from '../models/User';
 import { IUser } from '../models/IUser';
 // Interface do usuário, (o que o objeto espera receber)
 import UserValidations from './userValidations';
+// classe para validação de inputs
 import IConnection from '../models/IConnection';
 
 export default class UserController {
+
   constructor(private connection: IConnection) { }
   
 
+// 'Função 'createUser'
   // pra que que alguem vai usar esta funçao? FINALIDADE - CASO DE USO
   // quando a regra de negocio mudar, mude o controlador
   // validações nao podem ficar aqui porque fere o principio Single
   // ou seja, nem as 'req.body' podem ser definidas aqui porque se mudar o contrato de 'User'(o que ele recebe), teria uqe mudar aqui tambem
+  // todas as coisas que a funçao faz deve estar relacionado ao caso de uso
+  // CASO USO: criar um usuário
+  // checa validaçao, nao implementa validação
   createUser = async (req: Request, res: Response) => {
 
     if (UserValidations.validateUser(req.body as IUser)) {
--- block27-OOP-Solid/SOD-Principles/controllers/userValidations.ts
+++ block27-OOP-Solid/SOD-Principles/controllers/userValidations.ts
@@ -1,21 +1,31 @@
 import { IUser } from "../models/IUser";
 
+// CLASSE PARA VALIDAÇÃO
+// tipo de uso dessa classe: validação
+// classe abstrata para nao ser instaciada
+
 export default abstract class UserValidations {
+
+    // NAO TEM THIS NO MÉTODO, ELE DEVE SER ESTÁTICO
     static validateEmail = (email: string): boolean => {
         const emailRegex = /\S+@\S+\.\S/;
     
         return emailRegex.test(email);
       }
+    // quando por ex, tiver que mudar a validação da string, so muda aqui o codigoa
     
     static validatePassword = (password: string): boolean => {
         const passwordRegex = /^\d+$/; /* Senha pode ter apenas números */
 
         return passwordRegex.test(password);
     }
-
+     
+    // comparaçao booelana
     static validateRole = (role: string): boolean => {
         return role === 'admin' || role === 'user';
     }
+    // ta ruim isso! NAO É ESCALÁVEL
+    // sempre que surgir um papel novo no sistema essa funçao deve ser modificada -> 
 
     static validateUser = (user: IUser): boolean => {
         return UserValidations.validateEmail(user.email)
