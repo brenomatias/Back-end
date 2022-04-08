983506ba59c5669a20014a1886986bf342edbf13
brenoMatias <matiasbreno19@gmail.com>
2022-04-08 13:11:16 -0300

    adding base api

---
+++ block27-OOP-Solid/SOD-Principles/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import UserController from './controllers/userController';
+import MySQLConnection, { MockedConnection } from './models/connection';
+import IConnection from './models/IConnection';
+
+const app = express();
+
+app.use(express.json());
+
+
+let conn: IConnection;
+if (process.env.ENV === `devel`) {
+  conn = new MockedConnection();
+} else {
+  conn = MySQLConnection.conn;
+}
+
+
+app.post('/users', new UserController(new MockedConnection()).createUser);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
