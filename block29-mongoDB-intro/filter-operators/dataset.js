915978d6c6c9c819bbcdfc8e02ce7d1ad912275c
brenoMatias <matiasbreno19@gmail.com>
2022-05-17 15:22:02 -0300

    mongodb: refatoring to sharp code

---
+++ block29-mongoDB-intro/filter-operators/dataset.js
@@ -0,0 +1,36 @@
+use('films');
+
+db.films.insertMany([
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+     },
+     {
+        "Title": "I Am Legend",
+        "Year": "2007",
+        "Rated": "PG-13",
+        "Released": "14 Dec 2007",
+     },
+     {
+    
+        "Title": "300",
+        "Year": "2006",
+        "Rated": "R",
+        "Released": "09 Mar 2007",
+     },
+     {
+        "Title": "The Avengers",
+        "Year": "2012",
+        "Rated": "PG-13",
+        "Released": "04 May 2012",
+     },
+     {
+        "Title": "The Wolf of Wall Street",
+        "Year": "2013",
+        "Rated": "R",
+        "Released": "25 Dec 2013",
+     }    
+]);
+
